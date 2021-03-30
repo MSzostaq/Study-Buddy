423568132f8c029783b1f629e11b5ddd2e082f75
MSzostaq <maciek.szostak55@gmail.com>
2021-03-30 17:36:12 +0200

    themes - colors

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -3,7 +3,7 @@ export const theme = {
     black: "#111111",
     error: "#cb8581",
     grey: "#c0c7d6",
-    lightGrey: "#f7f8fa",
+    lightGrey: "#e0e0e0",
     succes: "#8fcb81",
     warning: "#e1d888",
     white: "#ffffff",
