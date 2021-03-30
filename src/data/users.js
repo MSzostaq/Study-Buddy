76fe652c88720ea269a62ef79157d191bdeb42cd
MSzostaq <maciek.szostak55@gmail.com>
2021-03-30 17:26:12 +0200

    changed , to .

--- src/data/users.js
+++ src/data/users.js
@@ -2,26 +2,26 @@ export const users = [
   {
     name: "Adam Romański",
     attendance: "38%",
-    average: "2,3",
+    average: "2.3",
   },
   {
     name: "Adam Małysz",
     attendance: "48%",
-    average: "3,2",
+    average: "3.2",
   },
   {
     name: "Robert Kubica",
     attendance: "50%",
-    average: "5,0",
+    average: "5.0",
   },
   {
     name: "Marian Romański",
     attendance: "12%",
-    average: "3,5",
+    average: "3.5",
   },
   {
     name: "Krzysztof Ibisz",
     attendance: "45%",
-    average: "4,3",
+    average: "4.3",
   },
 ];
