cfc01807c8c073c751951bc1d8c978338bf29d30
MSzostaq <maciek.szostak55@gmail.com>
2021-04-03 16:02:46 +0200

    new theme values

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -1,14 +1,18 @@
 export const theme = {
   colors: {
     black: "#111111",
+    darkGrey: "#737c8e",
+    darkPurple: "#c0c7d6",
     error: "#cb8581",
     grey: "#c0c7d6",
     lightGrey: "#e0e0e0",
+    lightPurple: "#eceff7",
     succes: "#8fcb81",
     warning: "#e1d888",
     white: "#ffffff",
   },
   fontSize: {
+    xl: "24px",
     l: "17px",
     m: "12px",
     s: "11px",
