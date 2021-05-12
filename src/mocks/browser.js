ed66d5c77ba5cf4c4782b4a5efa6362ec0ac15ed
MSzostaq <maciek.szostak55@gmail.com>
2021-05-12 08:49:39 +0200

    MSW Database - solution

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -4,10 +4,28 @@ import { db } from "mocks/db";
 
 export const worker = setupWorker(...handlers);
 
-const createStudents = () => {
+const seed = () => {
+  db.group.create({
+    id: "A",
+  });
+  db.group.create({
+    id: "B",
+  });
+  db.group.create({
+    id: "C",
+  });
+
   for (let i = 0; i < 15; i++) {
+    db.event.create();
     db.student.create();
   }
 };
 
-createStudents();
+seed();
+
+window.mocks = {
+  getEvents: () => db.event.getAll(),
+  getGroups: () => db.group.getAll(),
+  getStudents: () => db.student.getAll(),
+  seed,
+};
