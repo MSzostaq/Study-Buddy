f41d7000d8958c21055ba628fceb6fe2aeeaaa33
MSzostaq <maciek.szostak55@gmail.com>
2021-07-27 14:54:59 +0200

    atoms - code refactoring

--- src/components/atoms/Title.jsx
+++ src/components/atoms/Title.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Title = styled.h1`
   color: ${({ theme }) => theme.colors.darkGrey};
+  font-family: Montserrat, sans-serif;
   font-size: ${({ theme }) => theme.fontSize.xl};
 `;
 
